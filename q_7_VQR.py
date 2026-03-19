b5d4797f867ed2cf23303508b466a0cb616ef0b5
ForsIng <forsing@gmail.com>
2026-03-19 23:36:16 +0100

    Update q_7_VQR.py

--- q_7_VQR.py
+++ q_7_VQR.py
@@ -18,8 +18,8 @@ np.random.seed(SEED)
 random.seed(SEED)
 algorithm_globals.random_seed = SEED
 
-CSV_DRAWN = "/Users/4c/Desktop/GHQ/data/loto7hh_4582_k22.csv"
-CSV_ALL   = "/Users/4c/Desktop/GHQ/data/kombinacijeH_39C7.csv"
+CSV_DRAWN = "/data/loto7hh_4582_k22.csv"
+CSV_ALL   = "/data/kombinacijeH_39C7.csv"
 
 MIN_VAL = [1, 2, 3, 4, 5, 6, 7]
 MAX_VAL = [33, 34, 35, 36, 37, 38, 39]
@@ -209,7 +209,7 @@ Graf svih kombinacija: /Users/4c/Desktop/GHQ/data/kombinacijeH_39C7.csv
 
 ==================================================
 Predikcija (VQR, deterministicki, seed=39):
-[7, 10, 11, 23, 24, 25, 38]
+[7, 10, x, y, z, 25, 38]
 ==================================================
 """
 
